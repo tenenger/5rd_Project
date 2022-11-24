cc665b988b1b3ea29180281fe6dd563fe63d771f
Lee Dong Gyu <tenenger7125@gmail.com>
2022-11-24 23:37:58 +0900

    refactor: Update Favorite pages

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { SFavorite } from "./Favorite.style";
 import LoadingModal from "components/Loading/LoadingModal";
 import Location from "components/Location/Location";
 
-const likeList = [
+const DUMMY = [
   { sido: "서울", station: ["동대문구", "서초구"] },
   { sido: "대구", station: ["본동"] },
 ];
 
 const Favorite = () => {
   return (
-    <SFavorite>
-      {likeList.map((item) => (
+    <div>
+      {DUMMY.map((data) => (
         <Location
-          key={item.sido}
+          key={data.sido}
           loadingModal={<LoadingModal />}
-          sidoList={[item.sido]}
-          likeSido={item.sido}
-          station={item.station}
+          sidoList={[data.sido]}
+          likeSido={data.sido}
+          station={data.station}
           favorite
         />
       ))}
-    </SFavorite>
+    </div>
   );
 };
 
--- src/pages/Favorite/Favorite.style.js
+++
@@ -1,3 +0,0 @@
-import styled from "styled-components";
-
-export const SFavorite = styled.div``;
