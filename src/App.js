c0622e3f7a1cb657a4ad05c76698b0845738f68a
Lee Dong Gyu <tenenger7125@gmail.com>
2022-11-25 00:20:08 +0900

    refactor: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+
 import { Nav } from "pages/Nav/Nav";
 import { Favorite, MyLocation, AllLocation } from "pages/pages";
-import { SRoutes } from "./App.style";
 
-function App() {
+const App = () => {
   return (
     <>
-      <SRoutes>
-        <Routes>
-          <Route path="/" element={<MyLocation />} />
-          <Route path="/all" element={<AllLocation />} />
-          <Route path="/favorite" element={<Favorite />} />
-        </Routes>
-      </SRoutes>
+      <Routes>
+        <Route path="/" element={<MyLocation />} />
+        <Route path="/all" element={<AllLocation />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
       <Nav />
     </>
   );
