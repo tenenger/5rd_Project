78c4197989da3052fc85be1ffbb675dddfa1fd8e
Lee Dong Gyu <tenenger7125@gmail.com>
2022-12-06 02:38:34 +0900

    refactor: add try-catch

--- src/api/getGeolocation.js
+++ src/api/getGeolocation.js
@@ -6,8 +6,12 @@ const getGeolocation = async () => {
     );
   }
 
-  const data = await geolocation();
-  return await data.coords;
+  try {
+    const data = await geolocation();
+    return await data.coords;
+  } catch (err) {
+    throw new Error(err)
+  }
 };
 
 export default getGeolocation;
