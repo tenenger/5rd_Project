8fd91f51323013c7786e364c2d4e9a53fb58964e
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-09 04:14:18 +0900

    feat: add build

---
+++ dist/assets/LazySidoDustQuery-1057822f.js
@@ -0,0 +1,1 @@
+import{u as a,S as s,s as t,g as u}from"./index-96c78fe6.js";const r=e=>async()=>await u(e),o=()=>(a({queries:s.map(e=>({queryKey:[...t,e],queryFn:r(e),staleTime:3e5}))}),null);export{o as default};
