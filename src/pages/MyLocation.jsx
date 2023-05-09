526838144e82671830099a03908e685bf22674ad
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-09 10:51:51 +0900

    fix: deny location access

--- src/pages/MyLocation.jsx
+++ src/pages/MyLocation.jsx
@@ -7,7 +7,8 @@ import useSidoDust from '../hooks/useSidoDust';
 
 const MyLocation = () => {
   const { address } = useAddress();
-  const SIDO_NAME = SIDO_NAMES.find(SIDO_NAME => address?.[0].region_1depth_name.includes(SIDO_NAME));
+  const SIDO_NAME = SIDO_NAMES.find(SIDO_NAME => address?.[0].region_1depth_name.includes(SIDO_NAME)) ?? '서울';
+  console.log(SIDO_NAME);
   const { sidoDust, isLoading, error } = useSidoDust(SIDO_NAME);
 
   return <>{isLoading ? <LoadingModal /> : <WeatherCard isShow={false} data={sidoDust} />}</>;
