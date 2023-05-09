607c5c3405a7ec2540c7e0562ded7edbb68f5da2
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-10 00:20:41 +0900

    feat: add onclickoutside hook

---
+++ src/hooks/useOnClickOutSide.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useOnClickOutSide = handler => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (!ref.current || ref.current.contains(e.target)) return;
+
+      handler(e);
+    };
+
+    document.addEventListener('click', listener);
+
+    return () => {
+      document.removeEventListener('click', listener);
+    };
+  }, [ref]);
+
+  return ref;
+};
+
+export default useOnClickOutSide;
