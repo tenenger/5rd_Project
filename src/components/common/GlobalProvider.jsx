199ae18d9cc521c5e3c414f82ca789350aad4bb8
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-15 02:27:44 +0900

    feat: add color theme

--- src/components/common/GlobalProvider.jsx
+++ src/components/common/GlobalProvider.jsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalStyle from '../../styles/global.style';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from '../../constants/theme';
 
 export const queryClient = new QueryClient();
 
@@ -10,7 +12,9 @@ const GlobalProvider = ({ children }) => {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <GlobalStyle />
-      <RecoilRoot>{children}</RecoilRoot>
+      <ThemeProvider theme={defaultTheme}>
+        <RecoilRoot>{children}</RecoilRoot>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 };
