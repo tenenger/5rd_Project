f16667403d7c61df08ac811cb8dc21017f449234
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-22 02:19:59 +0900

    style: add main banner flex

--- src/components/common/dropdown/DropDown.style.js
+++ src/components/common/dropdown/DropDown.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SLayout = styled.div`
-  width: 100%;
+  position: relative;
 `;
 
 export const SDropDownSelect = styled.div`
--- src/pages/MyLocation/Banner.style.js
+++ src/pages/MyLocation/Banner.style.js
@@ -2,5 +2,8 @@ import styled from 'styled-components';
 
 export const SContainer = styled.div`
   height: 100%;
+  display: flex;
+  justify-content: space-between;
+
   padding: 20px 0;
 `;
