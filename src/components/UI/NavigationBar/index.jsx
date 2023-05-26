f17ec501f270a3d616f035e0933fc0be0a39f039
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-26 17:41:59 +0900

    feat: add search function and autocomplete to navigation bar

--- src/components/UI/NavigationBar/index.jsx
+++ src/components/UI/NavigationBar/index.jsx
@@ -1,17 +1,53 @@
+import { lazy, useMemo, useState } from 'react';
+
 import { Link } from 'react-router-dom';
+import { debounce } from 'lodash';
 
 import { Menu, Logo } from 'components/common';
 import { SearchIcon, UserIcon } from 'components/common/icons';
+import { useOnClickOutSide } from 'hooks';
 import { PATH } from 'constants';
 
 import { SLayout, SContainer, SUtils, SSearchBar, SearchInput } from './NavigationBar.style';
 
-const SearchBar = ({ icon }) => (
-  <SSearchBar size={icon.props.size}>
-    {icon}
-    <SearchInput placeholder="검색" size={icon.props.size} />
-  </SSearchBar>
-);
+const LazySearchAutoComplete = lazy(() => import('../lazySearchAutoComplete'));
+
+const SearchBar = ({ icon }) => {
+  const [isSearch, setIsSearch] = useState(false);
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const debounced = useMemo(() => debounce(value => setFilter(value), 300), []);
+
+  const handleSearchChange = e => {
+    const value = e.target.value.trim();
+
+    setSearch(value);
+    debounced(value);
+  };
+
+  const handleSearchFocus = () => setIsSearch(true);
+  const handleSearchBlur = () => setIsSearch(false);
+  const handleAutoCompleteItemClick = value => {
+    setSearch(value);
+    handleSearchBlur();
+  };
+  const ref = useOnClickOutSide(handleSearchBlur);
+
+  return (
+    <SSearchBar ref={ref} size={icon.props.size}>
+      {icon}
+      <SearchInput
+        placeholder="검색"
+        size={icon.props.size}
+        value={search}
+        onChange={handleSearchChange}
+        onFocus={handleSearchFocus}
+      />
+      <LazySearchAutoComplete filter={filter} isShow={isSearch} onItemClick={handleAutoCompleteItemClick} />
+    </SSearchBar>
+  );
+};
 
 const Utils = () => (
   <SUtils>
